b6a3c61d518212365e1b34501db7c0002624724f
Emma Tong <eltong@andrew.cmu.edu>
2024-09-12 16:38:59 -0400

    Finished InheritanceSortedIntList implementation. Fully tested.

--- Java/src/main/java/edu/cmu/cs/cs214/rec04/InheritanceSortedIntList.java
+++ Java/src/main/java/edu/cmu/cs/cs214/rec04/InheritanceSortedIntList.java
@@ -10,7 +10,43 @@ package edu.cmu.cs.cs214.rec04;
  *
  */
 
-public class InheritanceSortedIntList {
+public class InheritanceSortedIntList extends SortedIntList{
     // Write your implementation below with API documentation
+    private int totalAdded = 0;
 
+    /**
+     * Returns the total number of elements added to the list.
+     * @return the total number of elements added to the list
+     */
+    public int getTotalAdded() {
+        return totalAdded;
+    }
+
+    /**
+     * Adds the specified int to the list and updates totalAdded.
+     * @param num an integer to be added to the list
+     * @return true if the list is changed as a result of the call
+     */
+    @Override
+    public boolean add(int num) {
+        totalAdded++;
+        return super.add(num);
+    }
+
+    // /**
+    //  * Adds all of the elements of the IntegerList to the list and combines
+    //  * the totalAdded of both lists.
+    //  * @param list IntegerList containing elements to be added to the list
+    //  * @return true if the list changed as a result of the call
+    //  */
+    // @Override
+    // public boolean addAll(IntegerList list) {
+    //     if (super.addAll(list)) {
+    //         // totalAdded = list.size();
+    //         return true;
+    //     }
+    //     else {
+    //         return false;
+    //     }
+    // }
 }
